a5de06fd7d00ea0227ad459c097dc2e35298d976
abbymagicwhisper <aayanbl11@gmail.com>
2025-12-16 22:32:55 +0530

    Add files via upload

--- Class4 afterclass.py
+++ Class4 afterclass.py
@@ -1,6 +1,10 @@
-import math 
+#import math
 
-Squarenumber = int(input("Enter a number to find its square root:"))
-Squareroot = math.root(Squarenumber)
+#Squarenumber = int(input("Enter a number to find its square root:"))
+#Squareroot = math.sqrt(Squarenumber)
+#print("The square root of" , Squarenumber, "is " , Squareroot)
+
+Squarenumber = int(input("Enter a number to find its square:"))
+Squareroot = Squarenumber ** 0.5
 print("The square root of" , Squarenumber, "is " , Squareroot)
 
